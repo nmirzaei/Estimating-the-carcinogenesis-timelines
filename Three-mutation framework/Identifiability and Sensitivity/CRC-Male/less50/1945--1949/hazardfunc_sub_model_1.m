94379d28ddc8e3c8094594dc2cfd73123b939cf5
Navid M.Mirzaei <47603908+nmirzaei@users.noreply.github.com>
2026-03-13 18:55:33 -0400

    Add files via upload

---
+++ Three-mutation framework/Identifiability and Sensitivity/CRC-Male/less50/1945--1949/hazardfunc_sub_model_1.m
@@ -0,0 +1,8 @@
+function y = hazardfunc_sub_model_1(t,x,p,N0)
+    muN = p(1); mu1 = p(2); mu2 = p(3); alpha1=p(4); alpha2=p(5); beta1=p(6); beta2=p(7);
+    y = zeros(2,1);
+
+
+    y(1) = -muN*N0*x(1);
+    y(2) = 0; 
+end
